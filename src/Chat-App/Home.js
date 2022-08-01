8c3c8073f725afdd99529d40acb4f7a41d202631
your_github_username <harishguhan@dorustree.in>
2022-08-01 10:58:11 +0530

    Chat-App

--- src/Chat-App/Home.js
+++ src/Chat-App/Home.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import './ChatApp.css'
 const Home = () => {
   const [roomid, setRoomid] = useState("");
   const change = (e) => {
     setRoomid(e.target.value);
   };
   return (
-    <div className="container">
+    <div className="container home">
       <div className="row">
-        <div className="col-lg-12">
+        <div className="col-lg-12 home-card">
           <input
+            className="home-input"
             type="text"
             placeholder="Type Your Id"
             onChange={(e) => change(e)}
           />
-          <Link className="btn" to={`/${roomid}`}>Join Room</Link>
+          <Link className="btn home-button" to={`/${roomid}`}>Lets Chat</Link>
         </div>
       </div>
     </div>
