07c1ef1ccbbeae0597453bf67deac56d395aca1d
your_github_username <harishguhan@dorustree.in>
2022-08-03 17:00:10 +0530

    test-case

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -2,17 +2,42 @@ import React from "react";
 
 function LoginForm() {
   return (
-    <div className=''>
+    <div className="">
       <div className="row">
         <div className="col-lg-6">
           <form>
             <label>Name</label>
-            <input type="text" placeholder="enter the user name" name="name" data-testid="username" />
+            <input
+              type="text"
+              placeholder="enter the username"
+              name="name"
+              data-testid="username"
+            />
+            <label>Email</label>
+            <input 
+              type='email'
+              placeholder="Enter the Your email"
+              name="email"
+              data-testid="email"
+            />
+             <label>MobileNumber</label>
+            <input 
+              type='text'
+              placeholder="Enter the Your MobileNumber"
+              name="mobilenumber"
+              data-testid="mobilenumber"
+            />
+             <label>CityName</label>
+            <input 
+              type='text'
+              placeholder="Enter the Your CityName"
+              name="cityname"
+              data-testid="cityname"
+            />
           </form>
         </div>
       </div>
     </div>
-  
   );
 }
 
--- src/LoginForm.test.js
+++ src/LoginForm.test.js
@@ -6,7 +6,14 @@ import LoginForm from "./LoginForm";
 test('test the input fieds', () => {
     render(<LoginForm />);
 
-    const username = screen.getByTestId('username');
+    const username = screen.getByTestId('username');;
+    const email = screen.getByTestId('email');
+    const mobilenumber = screen.getByTestId('mobilenumber');
+    const cityname = screen.getByTestId('cityname')
+
     expect(username.textContent).toBe('');
+    expect(email.textContent).toBe('');
+    expect(mobilenumber.textContent).toBe('');
+    expect(cityname.textContent).toBe('');
 
 });
